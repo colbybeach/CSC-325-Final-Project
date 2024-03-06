a2263e326d4314ecb599d31a47339426d702b27f
Colby Beach <beachc@union.edu>
2024-03-06 12:44:39 -0500

    Work!

--- src/lane_and_avoid.py
+++ src/lane_and_avoid.py
@@ -16,11 +16,14 @@ OBSTACLE_DISTANCE_THRESHOLD = 0.5
 
 class LaneAndAvoid:
     def __init__(self):
+
         rospy.init_node('lane_and_avoid', anonymous=True)
-        self.velocity_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10) #topic to publish to, message type
-        self.laserScan_sub = rospy.Subscriber('/scan', LaserScan, self.scanCallback)
 
+        self.velocity_pub = rospy.Publisher("/cmd_vel", Twist, queue_size=10) 
+        self.laserScan_sub = rospy.Subscriber('/scan', LaserScan, self.scan_callback)
+        self.sub_lane = rospy.Subscriber('/detect/lane', Float64, self.detect_lane_callback, queue_size = 1)
 
+        self.twister = Twist()
         self.front = 0.0
         self.left = 0.0
         self.behind = 0.0
@@ -28,38 +31,43 @@ class LaneAndAvoid:
         self.yaw = 0.0
         self.nearing_object = False
 
-        self.twist = Twist()
         
-        
+        self.angular_z = 0.0  
         self.lastError = 0
-        self.sub_lane = rospy.Subscriber('/detect/lane', Float64, self.cbFollowLane, queue_size = 1)
 
 
-    def scanCallback(self, data):
+    def scan_callback(self, data):
 
         front_distances = data.ranges[-10:] + data.ranges[:10]
         self.front = np.mean([dist for dist in front_distances if not np.isinf(dist) and dist != 0])
 
-        left_distances = data.ranges[80:100]
-        self.left = np.mean([dist for dist in left_distances if not np.isinf(dist) and dist != 0])
+        # left_distances = data.ranges[80:100]
+        # self.left = np.mean([dist for dist in left_distances if not np.isinf(dist) and dist != 0])
 
-        behind_distances = data.ranges[170:190]
-        self.behind = np.mean([dist for dist in behind_distances if not np.isinf(dist) and dist != 0])
+        # behind_distances = data.ranges[170:190]
+        # self.behind = np.mean([dist for dist in behind_distances if not np.isinf(dist) and dist != 0])
 
-        right_distances = data.ranges[260:280]
-        self.right = np.mean([dist for dist in right_distances if not np.isinf(dist) and dist != 0])
+        # right_distances = data.ranges[260:280]
+        # self.right = np.mean([dist for dist in right_distances if not np.isinf(dist) and dist != 0])
 
 
         self.nearing_object = self.front < OBSTACLE_DISTANCE_THRESHOLD
 
-        print("Front: {:.2f}, Left: {:.2f}, Behind: {:.2f}, Right: {:.2f}".format(
-            data.ranges[0], data.ranges[90], data.ranges[180], data.ranges[270]))
+        # print("Front: {:.2f}, Left: {:.2f}, Behind: {:.2f}, Right: {:.2f}".format(
+        #     data.ranges[0], data.ranges[90], data.ranges[180], data.ranges[270]))
 
 
+    def stop_robot(self):
+        self.twister.linear.x = 0
+        self.twister.linear.y = 0
+        self.twister.linear.z = 0
+        self.twister.angular.x = 0
+        self.twister.angular.y = 0
+        self.twister.angular.z = 0
+        self.velocity_pub.publish(self.twister) 
 
 
-    def cbFollowLane(self, desired_center):
-
+    def detect_lane_callback(self, desired_center):
         center = desired_center.data
 
         error = center - 500
@@ -67,30 +75,30 @@ class LaneAndAvoid:
         Kp = 0.0025
         Kd = 0.007
 
-        angular_z = Kp * error + Kd * (error - self.lastError)
+        ang_z = Kp * error + Kd * (error - self.lastError)
+        self.angular_z = -max(ang_z, -2.0) if ang_z < 0 else -min(ang_z, 2.0)
         self.lastError = error
-        
 
 
-        if not self.nearing_object:
-            print("No object!")
-            self.twist.linear.x = 0.1
-            self.twist.linear.y = 0
-            self.twist.linear.z = 0
-            self.twist.angular.x = 0
-            self.twist.angular.y = 0
-            self.twist.angular.z = -max(angular_z, -2.0) if angular_z < 0 else -min(angular_z, 2.0)
-            self.velocity_pub.publish(self.twist)
-            rate = rospy.Rate(10)
 
-        else:
-            self.twist.linear.x = 0
-            print("Obstacle detected, rotating")
+    def run(self):
+        rate = rospy.Rate(10)  
+        while not rospy.is_shutdown():
+            if not self.nearing_object:
+                print("No object!")
+                self.twister.linear.x = 0.1
+                self.twister.angular.z = self.angular_z
+                self.velocity_pub.publish(self.twister)
+            else:
+                print("Obstacle detected, stopping.")
+                self.stop_robot()
 
-        rate.sleep()
+
+            rate.sleep()
 
 
 
 if __name__ == '__main__':
     laneAndAvoid = LaneAndAvoid()
+    laneAndAvoid.run()
     rospy.spin()
